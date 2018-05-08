8618f95b5b19ed5ab3ca1dd570d6d5adcf03512d
InfernoDeity <chorman64@gmail.com>
2018-05-08 16:03:31 -0400

    Update Stack.hpp

--- utils/Stack.hpp
+++ utils/Stack.hpp
@@ -1,9 +1,23 @@
 #ifndef __Stack_hpp_2018_05_07_15_29
 #define __Stack_hpp_2018_05_07_15_29
+namespace _hidden{
+	template<class target,int first,int... rest> struct pop{
+		static const constexpr int val = first;
+		typedef typename target<rest> next;
+	};
+
+	template<int val,class target,int... rest> struct push{
+		typedef typename target<val,first,rest> next; 
+	};
+};
 
 
 template<int... stackVals> struct stack{
-
+	template<int val> struct push: _hidden::push<val,stack,stackVals>{};
+	struct pop: _hidden::pop<stack,stackVals>{};
+};
+template<> struct stack<>{
+	template<int val> struct push: _hidden::push<val,stack>{};
 };
 
 #endif
