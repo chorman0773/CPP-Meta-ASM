86f0c7c77553754a5cc5811313909cfa0246e82f
InfernoDeity <chorman64@gmail.com>
2018-05-08 15:52:23 -0400

    Update Stack.hpp

--- utils/Stack.hpp
+++ utils/Stack.hpp
@@ -2,14 +2,8 @@
 #define __Stack_hpp_2018_05_07_15_29
 
 
-template<int top,int... rest> struct stack{
-	template<int val> struct push{
-		typedef stack<val,top,rest> nextStack;
-	};
-	struct pop{
-		static const constexpr int value = top;
-		typedef stack<rest> nextStack;
-	};
+template<int... stackVals> struct stack{
+
 };
 
 #endif
