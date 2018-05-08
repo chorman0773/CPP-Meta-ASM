cbfed09bc00c8453dc665af04c468a2a1b58367e
InfernoDeity <chorman64@gmail.com>
2018-05-08 17:20:43 -0400

    Update Program.hpp

--- asm/Program.hpp
+++ asm/Program.hpp
@@ -4,17 +4,23 @@
 #include <asm/Register.hpp>
 #include <utils/Stack.hpp>
 
-template<class stack,class accumulator,class nextInstruction,class... instructions> struct executing_program{
-  typedef nextInstruction::execute<stack,accumulator> currInstruction;
+template<class stack,class accumulator,class executingInstruction,class... instructions> struct executing_program{
+  typedef executingInstruction::execute<stack,accumulator> currInstruction;
   typedef executing_program<currInstruction::stack,currInstruction::accumulator,instructions> nextInstruction;
+  typedef nextInstruction::accumulator accumulator;
+  typedef nextInstruction::stack stack;
 };
 template<class stack,class accumulator,class lastInstruction> struct executing_program<stack,accumulator,lastInstruction>{
-  typedef nextInstruction::execute<stack,accumulator> currInstruction;
+  typedef lastInstruction::execute<stack,accumulator> currInstruction;
+  typedef currInstruction::accumulator accumulator;
+  typedef currInstruction::stack stack;
 };
 
 
 template<class... instructions> struct Program{
   typedef executing_program<Stack<>,Accumulator<0>,instructions> program;
+  typedef program::accumulator accumulator;
+  typedef program::stack stack;
 };
 
 #endif
