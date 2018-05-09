c2530102f83f6b75b72b2cc7329a8aa6dbd3cf03
InfernoDeity <chorman64@gmail.com>
2018-05-08 21:49:33 -0400

    Delete TypeExpansion.hpp

--- utils/TypeExpansion.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef __TypeExpansion_hpp_2018_05_08_16_15
-#define __TypeExpansion_hpp_2018_05_08_16_15
-
-template<typename T,T val> struct extend{
-	const static constexpr int value = val;
-};
-
-#endif
