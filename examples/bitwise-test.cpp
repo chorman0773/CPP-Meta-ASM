d87530d1052db7f22fc76bee83e7878a7a066134
InfernoDeity <chorman64@gmail.com>
2018-05-09 08:52:16 -0400

    Create bitwise-test.cpp

---
+++ examples/bitwise-test.cpp
@@ -0,0 +1,21 @@
+#include <asm/Program.hpp>
+#include <iostream>
+
+#define SEED 288149951
+#define MAX 10
+
+using std::cout;
+using std::endl;
+
+typedef Program<
+Subroutine<
+load<b,1>,
+push<b>,
+pop<b>,
+mul<a,516599491>,
+add<a,11>,
+add<b,1>,
+sub<b,MAX>,
+brn<-5>
+>
+Accumulator<START>>
