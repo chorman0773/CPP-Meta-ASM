9cf1007f261537cb712ba02ecf58e2d8dd87e337
InfernoDeity <chorman64@gmail.com>
2018-05-08 21:44:54 -0400

    Update test1.cpp

--- examples/test1.cpp
+++ examples/test1.cpp
@@ -6,10 +6,12 @@ using std::cout;
 using std::endl;
 
 typedef Program<
-load<a,1>,
-push<a>,
-mul<a,2>,
-brnz<-2>
+Subroutine<
+  load<a,1>,
+  push<a>,
+  mul<a,2>,
+  brnz<-2>
+ >
 > output;
 
 int main(){
